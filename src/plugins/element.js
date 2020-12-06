d9cede18b0e19ed566288f4af16cf50519d4fc9c
xushaohua <1965331534@qq.com>
2020-12-06 14:41:17 +0800

    完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button,Form,FormItem,Input,Row,Message } from 'element-ui'
+//import { Button,Form,FormItem,Input } from 'element-ui'
+
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Row)
+// 需要全局挂载到原型
+Vue.prototype.$message = Message
